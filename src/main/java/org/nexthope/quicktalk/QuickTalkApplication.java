24bbe0f6f6c8e9d1fd8733a411e05182a2f511b7
aminhl <amin.hlel@esprit.tn>
2025-01-27 20:12:28 +0100

    init commit

---
+++ src/main/java/org/nexthope/quicktalk/QuickTalkApplication.java
@@ -0,0 +1,13 @@
+package org.nexthope.quicktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nexthope/quicktalk/QuickTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexthope.quicktalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
