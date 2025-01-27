c359ae8f7e8a0b8cc35efb5c8ef7bc8640b36d53
aminhl <amin.hlel@esprit.tn>
2025-01-27 23:14:29 +0100

    feat: entities auditing

--- src/main/java/org/nexthope/quicktalk/QuickTalkApplication.java
+++ src/main/java/org/nexthope/quicktalk/QuickTalkApplication.java
@@ -2,8 +2,10 @@ package org.nexthope.quicktalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class QuickTalkApplication {
 
     public static void main(String[] args) {
